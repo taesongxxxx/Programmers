1d1c1f028aa822cde4986b54ef855b5ef4ffaa3d
맥북 프로16 <a2485@maegbug-ui-MacBookPro.local>
2026-03-31 21:08:20 +0900

    feat: book-store/ Title, Button, Input Component Create

--- book-store/src/components/common/Footer.tsx
+++ book-store/src/components/common/Footer.tsx
@@ -1,12 +1,43 @@
+import styled from "styled-components";
+import logo from "../../assets/images/logo.png";
+
 function Footer() {
   return (
-    <>
-      <hr />
-      <footer>
-        copyright(c), 2026, book sotre.
-      </footer>
-    </>
+    <FooterStyle>
+      <h1 className="logo">
+        <img src={logo} alt="book store"/>
+      </h1>
+      <div className="copyright">
+        <p>
+          copyright(c), 2026, book sotre.
+        </p>
+      </div>
+    </FooterStyle>
   )
 }
+
+const FooterStyle = styled.footer`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme}) => theme.layout.width.large};
+  border-top: 1px solid ${({theme}) => theme.color.background};
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    img {
+      width: 140px;
+    }
+  }
+
+  .copyright {
+    p {
+      font-size: 0.75rem;
+      color: ${({theme}) => theme.color.text};
+    }
+  }
+`;
 
 export default Footer;
